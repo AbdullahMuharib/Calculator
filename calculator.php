3f6c216423ae09e0ee0fb6c83f417a4b4b978616
Abdullah Bin Muharib <Ab@hotmail.com>
2024-03-23 17:19:27 +0300

    Revert "Ddvddv"

--- calculator.php
+++ calculator.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--  HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII  -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
@@ -19,7 +20,7 @@
     $x=$_GET["x"];
     $y=$_GET["y"];
 
-    
+
     switch ($p) {
         case '+':
             $r=$x+$y;
