e13e6f312ab9435eaf0f3c60020ec4484ec5a2dc
Abdullah Bin Muharib <Ab@hotmail.com>
2024-03-23 16:31:17 +0300

    Revert "Add List"

---
+++ print.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hdnvjrknvjkfnjdfk</h1>
+    <a href="">vfjkdflmlkkldfv</a>
+    <br><br><br><br>
+    <br><br>
+    <ul>
+        <li>Hi</li>
+        <li>gg</li>
+    </ul>
+
+    
+</body>
+</html>
